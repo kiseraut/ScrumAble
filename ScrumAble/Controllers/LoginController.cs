a25871d021d2c72101980fe6ed795c53a87fd0a8
kiseraut <kiseraut@gmail.com>
2021-05-28 20:49:32 -0400

    Add project files.

---
+++ ScrumAble/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrumAble.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //return "Login Page";
+
+        //}
+
+        public IActionResult Register(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ ScrumAble/Data/ScrumAbleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ScrumAble.Models;
+
+namespace ScrumAble.Data
+{
+    public class ScrumAbleContext : DbContext
+    {
+        public ScrumAbleContext (DbContextOptions<ScrumAbleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ScrumAble.Models.Users> Users { get; set; }
+    }
+}
